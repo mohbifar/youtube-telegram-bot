033910bba45c32db6b76d779f98fc0d0dbc36e43
mohbifar <45327029+mohbifar@users.noreply.github.com>
2025-04-16 11:56:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,59 @@
-import os
-import asyncio
 import requests
-from telegram import Bot
+import time
+from googleapiclient.discovery import build
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHANNEL = "@ماجراجویی‌های_ورزشی"
-YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
-
-YOUTUBE_CHANNELS = [
-    "UCqxPjMDgtg85WWGUioyl3tg",
-    "UCDy24mzKpEtSm9Ugq8eY-7Q"
+telegram_token = '8005150199:AAHa5UL5uKikDytbICUk8qZmgQKKcExjW-Q'
+chat_id = '-1002637620980'
+youtube_api_key = 'AIzaSyAL2XAQi7bOFWglFDZ9fIxRT5YPHi7WreU'
+channels = [
+    'SlowMotionGymnastics-SMG',
+    'BEAUTYOFSPORTS-e3t',
+    'PhlowZone',
+    'JumperOneAthletics',
+    'thetennisview',
+    'wwsportss',
+    'ai.shutov',
+    'shutuber6',
+    'youngstars043',
+    'tracksportsent',
+    'RKAthletic',
+    'thebestgymnastics',
+    'daieb_ing',
+    'jennibelleboutique',
+    'YouarePowerful.Brave.Brilliant',
+    'surf-ladies'
 ]
 
-async def main():
-    bot = Bot(token=TOKEN)
-    while True:
-        try:
-            for channel in YOUTUBE_CHANNELS:
-                videos = get_videos(channel)
-                for video in videos:
-                    await bot.send_message(CHANNEL, video)  # اضافه کردن await
-            await asyncio.sleep(3600)
-        except Exception as e:
-            print(f"Error: {e}")
-            await asyncio.sleep(60)
+youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
-def get_videos(channel_id):
-    url = f"https://www.googleapis.com/youtube/v3/search?key={YOUTUBE_API_KEY}&channelId={channel_id}&part=snippet,id&order=date&maxResults=1"
-    response = requests.get(url).json()
-    return [f"https://youtu.be/{item['id']['videoId']}" for item in response.get('items', [])]
+def get_latest_video(channel_id):
+    request = youtube.search().list(
+        part="snippet",
+        channelId=channel_id,
+        order="date",
+        maxResults=1
+    )
+    response = request.execute()
+    video = response['items'][0]
+    video_id = video['id']['videoId']
+    video_title = video['snippet']['title']
+    video_url = f'https://www.youtube.com/watch?v={video_id}'
+    return video_title, video_url
+
+def send_to_telegram(video_title, video_url):
+    message = f"{video_title}\n{video_url}"
+    url = f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chat_id}&text={message}"
+    requests.get(url)
+
+def run_bot():
+    processed_videos = set()
+    while True:
+        for channel in channels:
+            video_title, video_url = get_latest_video(channel)
+            if video_url not in processed_videos:
+                send_to_telegram(video_title, video_url)
+                processed_videos.add(video_url)
+        time.sleep(3600)
 
 if __name__ == "__main__":
-    asyncio.run(main())  # اجرای تابع async
+    run_bot()
